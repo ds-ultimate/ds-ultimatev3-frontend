45177c0b8fecbd333732ab20a1690d747c47e6fb
extremeCrazyCoder <MKich@gmx.at>
2023-02-06 12:05:21 +0100

    Forgot to commit that

--- src/app/(main_app)/navbar.langSelector.tsx
+++ src/app/(main_app)/navbar.langSelector.tsx
@@ -1,15 +1,19 @@
 'use client';
 import {useRouter} from "next/router";
+import Link from "next/link";
 
 export default function NavbarLangSelector({languages}: {languages: string[]}) {
+
+  /*
   const router = useRouter()
   const { pathname, asPath, query } = router
-
+  <a key={lang} onClick={() => router.push({pathname, query}, asPath, {locale: lang})}>{lang}</a>
+   */
   return (
       <>
         {languages.map(lang => {
           return (
-              <a key={lang} onClick={() => router.push({pathname, query}, asPath, {locale: lang})}>{lang}</a>
+              <Link key={lang} href={"/test"} locale={lang}>{lang}</Link>
           )
         })}
       </>
